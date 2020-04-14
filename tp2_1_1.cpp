bb58100038b484c6cd4fc106e121e16b6804676c
NataliaGabriela <natigab.24@gmail.com>
2020-04-14 01:30:59 -0300

    subiendo punto 2a)

---
+++ tp2_1_1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 4
+#define M 5
+
+
+int main(){
+int f,c;
+double mt[N][M];
+
+for(f = 0;f<N; f++)
+{
+ for(c = 0;c<M; c++)
+{
+printf("%lf ", mt[f][c]);
+}
+printf("\n");
+}
+scanf(" %c");
+return 0;
+}
